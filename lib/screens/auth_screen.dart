e233f69aaa293d6c4f88a88f31c084181f8dd7cc
Eli Hermann <Eli_Hermann@hotmail.com>
2023-02-12 21:58:50 -0700

    very basic authentication form that isn't hooked up to the backend yet

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,19 @@
+import 'package:endeavor/widgets/auth/auth_form.dart';
+
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: AuthForm(),
+    );
+  }
+}
