a3cdeaba4fb45717ff7c746f6deee68cc5cdcc4d
Eli Hermann <Eli_Hermann@hotmail.com>
2023-02-12 21:58:50 -0700

    very basic authentication form that isn't hooked up to the backend yet

--- README.md
+++ README.md
@@ -96,4 +96,6 @@ I'm using pycharm on my windows computer and the 3.8 interpreter in a virtual en
 
 
 ## What I'm working on now
-* I'm going to start with authentication so that I can get that structure into the ground floor.
+I'm following [this](https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/19510418#search) tutorial and just created a basic auth page.
+
+Next, to hook it up to the backend.
---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLoginMode = true;
+  var _userEmail = '';
+  var _userPassword = '';
+  var _userName = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState?.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid != null && isValid) {
+      _formKey.currentState?.save();
+      debugPrint(_userEmail);
+      debugPrint(_userName);
+      debugPrint(_userPassword);
+
+      // use these values to send our auth request ...
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    key: const ValueKey("email"),
+                    validator: (value) {
+                      if (value == null || !value.contains('@')) {
+                        return "Please enter a valid email address.";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: "Email Address",
+                    ),
+                    onSaved: (value) {
+                      if (value != null) {
+                        _userEmail = value;
+                      }
+                    },
+                  ),
+                  if (!_isLoginMode)
+                    TextFormField(
+                      key: const ValueKey("username"),
+                      validator: (value) {
+                        if (value == null) {
+                          return "Must enter some username";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(labelText: "Username"),
+                      onSaved: (value) {
+                        if (value != null) {
+                          _userName = value;
+                        }
+                      },
+                    ),
+                  TextFormField(
+                    key: const ValueKey("password"),
+                    validator: (value) {
+                      if (value == null || value.length < 7) {
+                        return "Password must be at least 7 charachters long";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(labelText: "Password"),
+                    obscureText: true,
+                    onSaved: (value) {
+                      if (value != null) {
+                        _userPassword = value;
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  ElevatedButton(
+                    child: Text(_isLoginMode ? "Login" : "Signup"),
+                    onPressed: () {
+                      _trySubmit();
+                    },
+                  ),
+                  TextButton(
+                    child: Text(
+                      _isLoginMode
+                          ? "Create Account"
+                          : "I Already Have an Account",
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isLoginMode = !_isLoginMode;
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
