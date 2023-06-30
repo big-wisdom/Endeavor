7079847905032346f1f850dce5eaafd3a5cf864b
Eli Hermann <Eli_Hermann@hotmail.com>
2023-06-30 09:57:15 -0700

    back to desktopg

--- packages/data_repository/lib/src/models/endeavor_block/abstract_endeavor_block/abstract_single_endeavor_block/abstract_single_endeavor_block.dart
+++ packages/data_repository/lib/src/models/endeavor_block/abstract_endeavor_block/abstract_single_endeavor_block/abstract_single_endeavor_block.dart
@@ -2,11 +2,13 @@ import 'package:data_repository/data_repository.dart';
 
 abstract class AbstractSingleEndeavorBlock extends AbstractEndeavorBlock {
   final String? repeatingEndeavorBlockId;
+  final Event event;
 
-  AbstractSingleEndeavorBlock({
-    required super.title,
-    required this.repeatingEndeavorBlockId,
-    required super.endeavorReference,
-    required Event event,
-  }) : super(abstractEvent: event);
+  AbstractSingleEndeavorBlock(
+      {required super.title,
+      required this.repeatingEndeavorBlockId,
+      required super.endeavorReference,
+      required Event event})
+      : this.event = event,
+        super(abstractEvent: event);
 }
