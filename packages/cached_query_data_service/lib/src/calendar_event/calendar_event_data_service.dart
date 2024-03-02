5e4e7ce9bb3628007419f363ea60f5f4e92a80e1
Eli Hermann <eli_hermann@hotmail.com>
2024-03-02 10:50:06 -0700

    Add in cached query

--- packages/cached_query_data_service/lib/src/calendar_event/calendar_event_data_service.dart
+++ packages/cached_query_data_service/lib/src/calendar_event/calendar_event_data_service.dart
@@ -1,5 +1,20 @@
+import 'package:cached_query/cached_query.dart';
 import 'package:data_models/data_models.dart';
+import 'package:http/http.dart' as http;
 
 class CalendarEventDataService {
-  createCalendarEvent(UnidentifiedCalendarEvent calendarEvent) {}
+  createCalendarEvent(UnidentifiedCalendarEvent calendarEvent) async {
+    final query = Query(
+      key: "createEvent",
+      queryFn: () => http.post(
+        Uri.parse('http://localhost:8080/events'),
+        body: calendarEvent.toSqlJson(),
+      ),
+    );
+
+    final result = await query.result;
+    print(result);
+
+    return result;
+  }
 }
