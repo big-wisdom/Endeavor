6add695237be55714ecd5ef05c692ffa2d05e9f3
Eli Hermann <eli_hermann@hotmail.com>
2024-03-02 22:31:28 -0700

    Create event through Go service

--- packages/cached_query_data_service/lib/src/calendar_event/calendar_event_data_service.dart
+++ packages/cached_query_data_service/lib/src/calendar_event/calendar_event_data_service.dart
@@ -1,19 +1,23 @@
+import 'dart:convert';
+
 import 'package:cached_query/cached_query.dart';
+import 'package:cached_query_data_service/cached_query_data_service.dart';
 import 'package:data_models/data_models.dart';
+import 'package:date_and_time_utilities/date_and_time_utilities.dart';
 import 'package:http/http.dart' as http;
 
 class CalendarEventDataService {
   createCalendarEvent(UnidentifiedCalendarEvent calendarEvent) async {
     final query = Query(
-      key: "createEvent",
-      queryFn: () => http.post(
-        Uri.parse('http://localhost:8080/events'),
-        body: calendarEvent.toSqlJson(),
-      ),
+      key:
+          "createEvent-${calendarEvent.event.start.toSqlString()}-${calendarEvent.event.end.toSqlString()}",
+      queryFn: () => http.post(Uri.parse('http://192.168.1.49:8080/events'),
+          body: jsonEncode(
+              calendarEvent.toSqlJson(CachedQueryDataService.userId)),
+          headers: {'Content-Type': "application/json"}),
     );
 
     final result = await query.result;
-    print(result);
 
     return result;
   }
