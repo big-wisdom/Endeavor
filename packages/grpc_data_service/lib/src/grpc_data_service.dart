52742b0cbe58de5160754bad0fc21f4db50a6efa
Eli Hermann <eli_hermann@hotmail.com>
2024-06-15 16:05:58 -0600

    Put CreateEndeavorBlock functionality in

--- packages/grpc_data_service/lib/src/grpc_data_service.dart
+++ packages/grpc_data_service/lib/src/grpc_data_service.dart
@@ -1,4 +1,5 @@
 import 'package:grpc/grpc.dart';
+import 'package:grpc_data_service/src/endeavor_blocks/endeavor_blocks_data_service.dart';
 import 'package:grpc_data_service/src/generated_protos/endeavor/service/endeavor_service.pbgrpc.dart';
 import './calendar_event/calendar_event_data_service.dart';
 
@@ -11,23 +12,28 @@ class GRPCDataService {
   static GRPCDataService instance = GRPCDataService._internal(); // init once
 
   late CalendarEventDataService calendarEvents;
+  late EndeavorBlocksDataService endeavorBlocks;
 
   String _baseUrl = "localhost";
   int _port = 8080;
 
   init(String userId) async {
+    final client = EndeavorClient(ClientChannel(
+      _baseUrl,
+      port: _port,
+
+      ///use credentials: ChannelCredentials.insecure() if you want to connect without Tls
+      options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
+
+      ///use this if you are connecting with Tls
+      // options: const ChannelOptions(),
+    ));
     calendarEvents = CalendarEventDataService(
-      EndeavorClient(ClientChannel(
-        _baseUrl,
-        port: _port,
-
-        ///use credentials: ChannelCredentials.insecure() if you want to connect without Tls
-        options:
-            const ChannelOptions(credentials: ChannelCredentials.insecure()),
-
-        ///use this if you are connecting with Tls
-        // options: const ChannelOptions(),
-      )),
+      client,
+      userId,
+    );
+    endeavorBlocks = EndeavorBlocksDataService(
+      client,
       userId,
     );
   }
