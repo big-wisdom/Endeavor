577aa321b3c6da0d929f2eb4fd1666a3da550f19
Eli Hermann <eli_hermann@hotmail.com>
2024-07-14 17:07:01 -0600

    Create, Edit, and Delete endeavorBlocks

--- packages/grpc_data_service/lib/src/endeavor_blocks/endeavor_blocks_data_service.dart
+++ packages/grpc_data_service/lib/src/endeavor_blocks/endeavor_blocks_data_service.dart
@@ -1,4 +1,6 @@
+import 'package:cached_query_flutter/cached_query_flutter.dart';
 import 'package:data_models/data_models.dart';
+import 'package:grpc_data_service/src/calendar_event/calendar_event_data_service.dart';
 import 'package:grpc_data_service/src/generated_protos/endeavor/service/endeavor_service.pbgrpc.dart';
 import 'package:grpc_data_service/src/generated_protos/google/protobuf/timestamp.pb.dart';
 import '../generated_protos/common_models/event.pb.dart' as EventProto;
@@ -7,10 +9,20 @@ import '../generated_protos/common_models/endeavorReference.pb.dart' as ER;
 class EndeavorBlocksDataService {
   EndeavorClient client;
   String _userId;
-  EndeavorBlocksDataService(this.client, String userId) : _userId = userId;
+
+  // EndeavorBlock Mutations
+  Mutation<bool, CreateEventRequest> _createMutation;
+
+  EndeavorBlocksDataService(this.client, String userId)
+      : _userId = userId,
+        _createMutation = Mutation(
+          refetchQueries: [CalendarEventDataService.eventsQueryId],
+          queryFn: (createEventRequest) =>
+              client.createEvent(createEventRequest).then((v) => true),
+        );
 
   void CreateEndeavorBlock(UnidentifiedEndeavorBlock endeavorBlock) {
-    client.createEvent(
+    _createMutation.mutate(
       CreateEventRequest(
         event: EventProto.Event(
           userId: _userId,
