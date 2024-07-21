c62beee0cb880df4a91f6cd93821bd4c9e25fff6
Eli Hermann <eli_hermann@hotmail.com>
2024-07-21 14:33:39 -0600

    Add time zone support

--- packages/grpc_data_service/lib/src/generated_protos/common_models/repeating_event.pbjson.dart
+++ packages/grpc_data_service/lib/src/generated_protos/common_models/repeating_event.pbjson.dart
@@ -24,15 +24,16 @@ const RepeatingEvent$json = {
     {'1': 'endeavorId', '3': 5, '4': 1, '5': 5, '10': 'endeavorId'},
     {'1': 'startTime', '3': 6, '4': 1, '5': 11, '6': '.common_models.Time', '10': 'startTime'},
     {'1': 'endTime', '3': 7, '4': 1, '5': 11, '6': '.common_models.Time', '10': 'endTime'},
-    {'1': 'startDate', '3': 8, '4': 1, '5': 11, '6': '.google.protobuf.Timestamp', '10': 'startDate'},
-    {'1': 'endDate', '3': 9, '4': 1, '5': 11, '6': '.google.protobuf.Timestamp', '10': 'endDate'},
-    {'1': 'm', '3': 10, '4': 1, '5': 8, '10': 'm'},
-    {'1': 't', '3': 11, '4': 1, '5': 8, '10': 't'},
-    {'1': 'w', '3': 12, '4': 1, '5': 8, '10': 'w'},
-    {'1': 'th', '3': 13, '4': 1, '5': 8, '10': 'th'},
-    {'1': 'f', '3': 14, '4': 1, '5': 8, '10': 'f'},
-    {'1': 's', '3': 15, '4': 1, '5': 8, '10': 's'},
-    {'1': 'su', '3': 16, '4': 1, '5': 8, '10': 'su'},
+    {'1': 'startDate', '3': 8, '4': 1, '5': 11, '6': '.common_models.Date', '10': 'startDate'},
+    {'1': 'endDate', '3': 9, '4': 1, '5': 11, '6': '.common_models.Date', '10': 'endDate'},
+    {'1': 'hoursOffset', '3': 10, '4': 1, '5': 5, '10': 'hoursOffset'},
+    {'1': 'm', '3': 11, '4': 1, '5': 8, '10': 'm'},
+    {'1': 't', '3': 12, '4': 1, '5': 8, '10': 't'},
+    {'1': 'w', '3': 13, '4': 1, '5': 8, '10': 'w'},
+    {'1': 'th', '3': 14, '4': 1, '5': 8, '10': 'th'},
+    {'1': 'f', '3': 15, '4': 1, '5': 8, '10': 'f'},
+    {'1': 's', '3': 16, '4': 1, '5': 8, '10': 's'},
+    {'1': 'su', '3': 17, '4': 1, '5': 8, '10': 'su'},
   ],
 };
 
@@ -42,11 +43,11 @@ final $typed_data.Uint8List repeatingEventDescriptor = $convert.base64Decode(
     'QSFAoFdGl0bGUYAyABKAlSBXRpdGxlEigKD2lzRW5kZWF2b3JCbG9jaxgEIAEoCFIPaXNFbmRl'
     'YXZvckJsb2NrEh4KCmVuZGVhdm9ySWQYBSABKAVSCmVuZGVhdm9ySWQSMQoJc3RhcnRUaW1lGA'
     'YgASgLMhMuY29tbW9uX21vZGVscy5UaW1lUglzdGFydFRpbWUSLQoHZW5kVGltZRgHIAEoCzIT'
-    'LmNvbW1vbl9tb2RlbHMuVGltZVIHZW5kVGltZRI4CglzdGFydERhdGUYCCABKAsyGi5nb29nbG'
-    'UucHJvdG9idWYuVGltZXN0YW1wUglzdGFydERhdGUSNAoHZW5kRGF0ZRgJIAEoCzIaLmdvb2ds'
-    'ZS5wcm90b2J1Zi5UaW1lc3RhbXBSB2VuZERhdGUSDAoBbRgKIAEoCFIBbRIMCgF0GAsgASgIUg'
-    'F0EgwKAXcYDCABKAhSAXcSDgoCdGgYDSABKAhSAnRoEgwKAWYYDiABKAhSAWYSDAoBcxgPIAEo'
-    'CFIBcxIOCgJzdRgQIAEoCFICc3U=');
+    'LmNvbW1vbl9tb2RlbHMuVGltZVIHZW5kVGltZRIxCglzdGFydERhdGUYCCABKAsyEy5jb21tb2'
+    '5fbW9kZWxzLkRhdGVSCXN0YXJ0RGF0ZRItCgdlbmREYXRlGAkgASgLMhMuY29tbW9uX21vZGVs'
+    'cy5EYXRlUgdlbmREYXRlEiAKC2hvdXJzT2Zmc2V0GAogASgFUgtob3Vyc09mZnNldBIMCgFtGA'
+    'sgASgIUgFtEgwKAXQYDCABKAhSAXQSDAoBdxgNIAEoCFIBdxIOCgJ0aBgOIAEoCFICdGgSDAoB'
+    'ZhgPIAEoCFIBZhIMCgFzGBAgASgIUgFzEg4KAnN1GBEgASgIUgJzdQ==');
 
 @$core.Deprecated('Use timeDescriptor instead')
 const Time$json = {
@@ -60,4 +61,19 @@ const Time$json = {
 /// Descriptor for `Time`. Decode as a `google.protobuf.DescriptorProto`.
 final $typed_data.Uint8List timeDescriptor = $convert.base64Decode(
     'CgRUaW1lEhQKBWhvdXJzGAEgASgFUgVob3VycxIYCgdtaW51dGVzGAIgASgFUgdtaW51dGVz');
+
+@$core.Deprecated('Use dateDescriptor instead')
+const Date$json = {
+  '1': 'Date',
+  '2': [
+    {'1': 'day', '3': 1, '4': 1, '5': 5, '10': 'day'},
+    {'1': 'month', '3': 2, '4': 1, '5': 5, '10': 'month'},
+    {'1': 'year', '3': 3, '4': 1, '5': 5, '10': 'year'},
+  ],
+};
+
+/// Descriptor for `Date`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List dateDescriptor = $convert.base64Decode(
+    'CgREYXRlEhAKA2RheRgBIAEoBVIDZGF5EhQKBW1vbnRoGAIgASgFUgVtb250aBISCgR5ZWFyGA'
+    'MgASgFUgR5ZWFy');
 
